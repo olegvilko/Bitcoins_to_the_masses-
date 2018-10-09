a1898734a319f2c00c9212f5f7b05446d6f31ba0
vilko <helgi8@mail.ru>
2018-10-09 04:27:10 +0300

    Добавьте файлы проекта.

---
+++ Bitcoins_to_the_masses!/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bitcoins_to_the_masses_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double amount = 1.11; //количество биткоинов от одного человека
+            int peopleCount = 60; // количество человек
+            int totalMoney = (int)(Math.Round(amount * peopleCount)); // ← исправьте ошибку в этой строке
+            Console.WriteLine(totalMoney);
+            Console.ReadKey();
+        }
+    }
+}
